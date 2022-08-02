8e296facd4ef068ac978f2da9953bda91ddc6a95
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-08-02 09:32:36 -0500

    “pimer commitâ

---
+++ movie/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):  
+  #return HttpResponse('<h1>Welcome to Home</h1>')
+  #return render(request, 'home.html')
+  return render(request, 'home.html', {'name': 'Andres Guerra'})
+
