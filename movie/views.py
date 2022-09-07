6c8884b534c7e0b41797ec093a4e502a8dfe9d1c
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-09-07 13:09:01 -0500

    solucion en error de busqueda

--- movie/views.py
+++ movie/views.py
@@ -8,7 +8,7 @@ def home(request):
   #return render(request, 'home.html')
   searchTerm = request.GET.get('searchMovie')
   if searchTerm:
-    movies = Movie.objects.filter(title_icontains=searchTerm)
+    movies = Movie.objects.filter(title__icontains=searchTerm)
   else:
     movies = Movie.objects.all()
   return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
